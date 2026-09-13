d48a5068c49765cd1e4487326bc9b568651d179d
gwadsfwascawfwga <gwadsfwascawfwga@mail.ru>
2026-09-13 20:20:35 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    puts("       /|  ");
+    puts("      / |  ");
+    puts("     /  |  ");
+    puts("    /___|  ");
+    puts("      |    ");
+    puts(" \\________/");
+    puts("  ~~~~~~~~ ");
+
+    return 0;
+}
